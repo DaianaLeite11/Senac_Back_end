8dc669e91580b2475d833463619ac5a61e5ea87c
Daiana Leite <daianaleite11@gmail.com>
2024-10-24 20:54:01 -0300

    aula08

--- aula08/Professor.php
+++ aula08/Professor.php
@@ -1,4 +1,30 @@
 <?php 
 
+class Professor {
+
+    private $nome;
+    private $cpf;
+    private $disciplina;
+    private $identificacao;
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function getDisciplina(){
+        return $this->disciplina;
+    }
+
+    public function setDisciplina($disciplinaNova){
+
+        $this->disciplina = $disciplinaNova;
+    }
+
+    public function setCpf($cpfNovo){
+
+        $this->cpf = $cpfNovo;
+    }
+
+}
 
 ?>
