6531372e076654412b1a22fdd14ad3d5bafd4110
Daiana Leite <daianaleite11@gmail.com>
2024-10-29 20:26:49 -0300

    aula10

--- aula10/Aluno.php
+++ aula10/Aluno.php
@@ -8,8 +8,8 @@ class Aluno extends Pessoa
     private $notas = [];
     private $rg;
 
-    public function __construct($nome, $dataDeNascimento, $disciplina, $rg){
-        parent::__construct();
+    public function __construct($nome, $dataDeNascimento, $disciplina, $cpf, $identificacao, $rg){
+        parent::__construct($nome, $dataDeNascimento, $disciplina, $cpf, $identificacao);
         $this->rg = $rg;
     }
     
